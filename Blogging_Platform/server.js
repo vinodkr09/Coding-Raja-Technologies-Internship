48d2d2673d0f944bc0ea7e69bf6ca54cabdf3a2a
vinodkr09 <vinod.krmahi@gmail.com>
2024-01-12 05:00:39 +0530

    code are push to github

--- Blogging_Platform/server.js
+++ Blogging_Platform/server.js
@@ -1,6 +1,7 @@
  //importing all packages
  const express = require('express');
  const path = require('path');
+ const fileupload = require('express-fileupload')
 
  //store "public" folder path to initial_path variable
  let initial_path = path.join(__dirname, "public");
@@ -8,10 +9,37 @@
  //initializing the Express server
  const app = express();
  app.use(express.static(initial_path));
+ app.use(fileupload())
  
  app.get('/', (req, res) => {
     res.sendFile(path.join(initial_path, "home.html"));
  })
+
+ app.get('/editor', (req, res) => {
+   res.sendFile(path.join(initial_path, "editor.html"));
+})
+
+ //upload link
+ app.post('/upload', (req, res) => {
+   let file = req.files.image;
+   let date = new Date();
+   // image name
+   let imagename = date.getDate() + date.getTime() + file.name;
+   // image upload path
+   let path = 'public/uploads/' + imagename;
+
+   // create upload
+   file.mv(path, (err, result) => {
+       if(err){
+           throw err;
+       } else{
+           // our image upload path
+           res.json(`uploads/${imagename}`)
+       }
+   })
+})
+
+
 
  app.listen("3000", () => {
     console.log("Server is running on port 3000")
